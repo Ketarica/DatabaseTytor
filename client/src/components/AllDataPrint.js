c6e03837ec876e78e8c86d8217bac66e7cba1128
Ketarica <120320477+Ketarica@users.noreply.github.com>
2024-10-11 09:57:42 +0300

    Module (#3)
    * new search version
    
    * nodule update

--- client/src/components/AllDataPrint.js
+++ client/src/components/AllDataPrint.js
@@ -1,14 +1,17 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import UpdateData from "./UpdateData";
 import SearchGame from "./SearchGame";
+
+import BlogPage from "./BlogPage";
+
+
 const img = require('../img/placeholderFilm.png');
 
 const AllDataPrint = () => {
     const [games, setGames] = useState([]);
 
     const [filteredGames, setFilteredGames] = useState([]);
-
-   
 
 
     const getGames = async () => {
@@ -19,9 +22,8 @@ const AllDataPrint = () => {
             const dataFormat = await serverCall.json();
             setGames(dataFormat);
 
-            setFilteredGames(dataFormat); 
+            setFilteredGames(dataFormat);
         } catch (error) {
-
 
             console.error(error);
         }
@@ -29,6 +31,7 @@ const AllDataPrint = () => {
 
     const deleteData = async (id) => {
         try {
+
             await fetch(`http://localhost:5000/games/${id}`, {
                 method: "DELETE"
             });
@@ -43,34 +46,39 @@ const AllDataPrint = () => {
 
 
 
-useEffect(() => { getGames(); }, []);
+    useEffect(() => { getGames(); }, []);
 
-return (
-    <Fragment>
-        {/* Компонент поиска */}
-        <SearchGame games={games} setFilteredGames={setFilteredGames} />
-        <div className="card-list-title">Game List:</div>
-        <div className="game-list">
-            {filteredGames.map(game => (
-                <div className="game-item" key={game.game_id}>
-                    <div className="game-details">
-                        <img className="game-img" src={img} alt="Game Poster" />
-                        <p className="game-name">{game.name}</p>
-                        <p className="game-score">Score: {game.score}</p>
-                        <p className="game-description">{game.description}</p>
-                        <p className="game-release">Release: {game.release}</p>
+    return (
+        <Fragment>
+            {/* Компонент поиска */}
+            <div className="search-box"> <SearchGame games={games} setFilteredGames={setFilteredGames} /> </div>
+
+            <div className="card-list-title">Game List:</div>
+            <div className="game-list">
+                {filteredGames.map(game => (
+                    <div className="game-item" key={game.game_id}>
+                        <div className="game-details">
+                            <img className="game-img" src={img} alt="Game Poster" />
+                            <p className="game-name">{game.name}</p>
+                            <p className="game-score">Score: {game.score}</p>
+                            <p className="game-description">{game.description}</p>
+                            <p className="game-release">Release: {game.release}</p>
+                        </div>
+                        <div className="update-module">
+                            <UpdateData games={game} id={game.game_id} />
+                        </div>
+                        <button className="delete-button" onClick={() => deleteData(game.game_id)}>
+                            Delete
+                        </button>
                     </div>
-                    <div className="update-module">
-                        <UpdateData games={game} id={game.game_id} />
-                    </div>
-                    <button className="delete-button" onClick={() => deleteData(game.game_id)}>
-                        Delete
-                    </button>
-                </div>
-            ))}
-        </div>
-    </Fragment>
-);
+                ))}
+            </div>
+
+            <div>
+                <BlogPage />
+            </div>
+        </Fragment>
+    );
 };
 
 export default AllDataPrint;
---
+++ client/src/components/BlogPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const posts = [
+    { id: 1, title: 'First Post' },
+    { id: 2, title: 'Second Post' },
+];
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        {/* <Link to={`#`}>{post.title}</Link> */}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default BlogPage;
