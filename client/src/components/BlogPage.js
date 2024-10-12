cd468eeb1dcc736f81a1ed26a88098d8cc770741
Ketarica <laridiajin@gmail.com>
2024-10-12 22:22:53 +0300

    test

--- client/src/components/BlogPage.js
+++ client/src/components/BlogPage.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-const posts = [
-    { id: 1, title: 'First Post' },
-    { id: 2, title: 'Second Post' },
-];
+// const posts = [
+//     { id: 1, title: 'First Post' },
+//     { id: 2, title: 'Second Post' },
+// ];
 
-const BlogPage = () => {
-    return (
-        <div>
-            <h1>Blog</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        {/* <Link to={`#`}>{post.title}</Link> */}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+// const BlogPage = () => {
+//     return (
+//         <div>
+//             <h1>Blog</h1>
+//             <ul>
+//                 {posts.map((post) => (
+//                     <li key={post.id}>
+//                         {/* <Link to={`#`}>{post.title}</Link> */}
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
 
-export default BlogPage;
+// export default BlogPage;
