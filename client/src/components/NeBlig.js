bda3a17f21940803c564c8fe50719b93a34b8964
Ketarica <laridiajin@gmail.com>
2024-10-12 22:32:42 +0300

    test

--- client/src/components/NeBlig.js
+++ client/src/components/NeBlig.js
@@ -1,20 +1,5 @@
-// import { Fragment } from 'react';
-// import React from 'react';
+import { Fragment } from 'react';
+import React from 'react';
 
-// const Header = () => {
-//     return (
-//         <div className="news-header">
-//             <div className="news-title">
-//                 <span className="separator">|</span>
-//                 <h1 className="news-subtitle">Games</h1>
-//             </div>
-//             <div className="mini-menu">
-//                 <a href="#">Home</a>
-//                 <span> → </span>
-//                 <a href="#">Games</a>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Header;
+const Header = () => { }
+export default Header;
