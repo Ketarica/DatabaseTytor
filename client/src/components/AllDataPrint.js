eedbe0323c49d75e0ba2b75526227404816d2235
Ketarica <laridiajin@gmail.com>
2024-11-15 15:16:06 +0300

    module fix for pictures

--- client/src/components/AllDataPrint.js
+++ client/src/components/AllDataPrint.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import UpdateData from "./UpdateData";
 import SearchGame from "./SearchGame";
-import GameForm from "./GameForm";
 
 const imgPlaceholder = require('../img/placeholderFilm.png');
 
@@ -30,16 +29,6 @@ const AllDataPrint = () => {
         }
     };
 
-    const handleImageUpload = (gameId, newImageUrl) => {
-
-        setGames(games.map(game =>
-            game.game_id === gameId ? { ...game, img: newImageUrl } : game
-        ));
-        setFilteredGames(filteredGames.map(game =>
-            game.game_id === gameId ? { ...game, img: newImageUrl } : game
-        ));
-    };
-
     useEffect(() => { getGames(); }, []);
 
     return (
@@ -54,22 +43,18 @@ const AllDataPrint = () => {
                     <div className="game-item" key={game.game_id}>
                         <div className="game-details">
                             <img
-                                src={game.img ? `http://localhost:5000/${game.img}` : imgPlaceholder}
+                                src={game.img ? `http://localhost:5000${game.img}` : imgPlaceholder}
                                 alt="Game Poster"
                                 className="game-img"
-                                style={{  width: '200px', height: '300px', maxWidth: '200px', maxHeight: '300px' }}
+                                style={{ width: '200px', height: '300px', maxWidth: '200px', maxHeight: '300px' }}
                             />
                             <p className="game-name">{game.name}</p>
                             <p className="game-score">Score: {game.score}</p>
                             <p className="game-description">{game.description}</p>
                             <p className="game-release">Release: {game.release}</p>
-
                         </div>
                         <div className="update-module">
-                            <GameForm
-                                gameId={game.game_id}
-                                onImageUpload={(newImageUrl) => handleImageUpload(game.game_id, newImageUrl)} />
-                            <UpdateData games={game} id={game.game_id} />
+                            <UpdateData games={game} />
                         </div>
                         <button className="delete-button" onClick={() => deleteData(game.game_id)}>
                             Delete
@@ -82,4 +67,3 @@ const AllDataPrint = () => {
 };
 
 export default AllDataPrint;
-
